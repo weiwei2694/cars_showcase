557da230fe077cd620d0bca6f007e1a623f4825a
Arya Priema <99aryaprima@gmail.com>
2023-06-14 17:39:57 +0700

    CarDetails

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -76,7 +76,7 @@ const CarCard = ({ car }: CarCardProps) => {
         </div>
       </div>
 
-      <CarDetails />
+      <CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car} />
     </div>
   );
 };
